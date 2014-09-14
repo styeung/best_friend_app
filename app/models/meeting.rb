70a38a7e3190126d1d9f8dbfe542fc99136aee68
Dan Vingo <danvingo@gmail.com>
2014-09-14 14:54:27 -0400

    Added meetings association

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,4 @@
 class Meeting < ActiveRecord::Base
+  #has_many :attended_users, through: :meeting_users
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,47 +2,47 @@ class User < ActiveRecord::Base
   validates :username, :email, :password_hash, presence: true
   validates :username, :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
-  
+
+  #has_many :saved_users, through: :saved_users
+  #has_many :ignored_users, through: :ignored_users
+  #has_many :conversations, through: :conversations_users
+  has_many :meetings, foreign_key: :creator_id
+
   attr_reader :password
-  
   after_initialize :ensure_session_token
-  
+
   def self.find_by_credentials(email, password)
     @user = User.find_by_email(email)
-    
+
     if @user
       if @user.is_password?(password)
         return @user
       end
     end
-    
+
     return nil
   end
-  
+
   def password=(password)
     @password = password
     self.password_hash = BCrypt::Password.create(password)
   end
-  
+
   def is_password?(password)
     BCrypt::Password.new(self.password_hash).is_password?(password)
   end
-  
+
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16)
   end
-  
+
   def reset_session_token
     self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
   end
-  
+
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
   end
-  
-  
-  
-  
 end
